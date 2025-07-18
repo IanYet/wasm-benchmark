2d806c9499383271a04d26df2517b1aec6ed5b97
IanYet <yt2015.97@gmail.com>
2025-07-18 17:01:26 +0800

    feat: type

--- src/utils.ts
+++ src/utils.ts
@@ -1,26 +1,25 @@
 /**
  * Vector2
  */
-export type V2 = Uint32Array & { length: 2 }
+export type V2 = [number, number]
 /**
  * Vector3
  */
-export type V3 = Uint32Array & { length: 3 }
+export type V3 = [number, number, number]
 /**
  * Matrix2x2
  */
-export type M2 = Uint32Array & { length: 4 }
+export type M2 = [number, number, number, number]
 /**
  * Matrix3x3
  */
-export type M3 = Uint32Array & { length: 9 }
+export type M3 = [number, number, number, number, number, number, number, number, number]
 
-export const v2 = (x: number, y: number): V2 => new Uint32Array([x, y]) as V2
+export const v2 = (x: number, y: number): V2 => [x, y]
 
-export const v3 = (x: number, y: number, z: number): V3 => new Uint32Array([x, y, z]) as V3
+export const v3 = (x: number, y: number, z: number): V3 => [x, y, z]
 
-export const m2 = (a11: number, a12: number, a21: number, a22: number): M2 =>
-	new Uint32Array([a11, a12, a21, a22]) as M2
+export const m2 = (a11: number, a12: number, a21: number, a22: number): M2 => [a11, a12, a21, a22]
 
 export const m3 = (
 	a11: number,
@@ -32,7 +31,7 @@ export const m3 = (
 	a31: number,
 	a32: number,
 	a33: number
-): M3 => new Uint32Array([a11, a12, a13, a21, a22, a23, a31, a32, a33]) as M3
+): M3 => [a11, a12, a13, a21, a22, a23, a31, a32, a33]
 
 export const applyM2 = (m: M2, v: V2): V2 => {
 	const x = m[0] * v[0] + m[1] * v[1]
