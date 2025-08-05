5a6354cc67942e892915a3121ff99fde18468683
IanYet <yt2015.97@gmail.com>
2025-08-05 16:15:53 +0800

    feat: perspective transform& anti alias

---
+++ src/transform.ts
@@ -0,0 +1,54 @@
+import { applyM3Inplace, lerpNumber, v3, type M3 } from './utils'
+
+export const perspectiveTransform = (
+	src: Uint8ClampedArray,
+	width: number,
+	height: number,
+	m: M3
+): Uint8ClampedArray => {
+	const dst = new Uint8ClampedArray(src.length)
+	const v = v3(0, 0, 1)
+
+	for (let i = 0; i < width; ++i) {
+		for (let j = 0; j < height; ++j) {
+			v[0] = i
+			v[1] = j
+			v[2] = 1
+			applyM3Inplace(m, v)
+			v[0] = v[0] / v[2]
+			v[1] = v[1] / v[2]
+			v[2] = 1
+
+			const x = Math.floor(v[0])
+			const y = Math.floor(v[1])
+
+			if (x < -1 || y < -1) {
+				dst[(j * width + i) * 4] = 0
+				dst[(j * width + i) * 4 + 1] = 0
+				dst[(j * width + i) * 4 + 2] = 0
+				dst[(j * width + i) * 4 + 3] = 0
+			} else {
+				const factorX = v[0] - x
+				const factorY = v[1] - y
+
+				for (let k = 0; k < 4; ++k) {
+					dst[(j * width + i) * 4 + k] = lerpNumber(
+						lerpNumber(
+							src[(y * width + x) * 4 + k],
+							src[(y * width + x + 1) * 4 + k],
+							factorX
+						),
+						lerpNumber(
+							src[((y + 1) * width + x) * 4 + k],
+							src[((y + 1) * width + x + 1) * 4 + k],
+							factorX
+						),
+						factorY
+					)
+				}
+			}
+		}
+	}
+
+	return dst
+}
--- src/utils.ts
+++ src/utils.ts
@@ -7,6 +7,10 @@ export type V2 = [number, number]
  */
 export type V3 = [number, number, number]
 /**
+ * Color
+ */
+export type Color = [number, number, number, number]
+/**
  * Matrix2x2
  */
 export type M2 = [number, number, number, number]
@@ -30,6 +34,16 @@ export const v3Inplace = (v: V3, x: number, y: number, z: number): V3 => {
 	v[1] = y
 	v[2] = z
 	return v
+}
+
+export const color = (r: number, g: number, b: number, a: number): Color => [r, g, b, a]
+
+export const colorInplace = (c: Color, r: number, g: number, b: number, a: number): Color => {
+	c[0] = r
+	c[1] = g
+	c[2] = b
+	c[3] = a
+	return c
 }
 
 export const m2 = (a11: number, a12: number, a21: number, a22: number): M2 => [a11, a12, a21, a22]
@@ -102,4 +116,49 @@ export const applyM3Inplace = (m: M3, v: V3): V3 => {
 	const y = m[3] * v[0] + m[4] * v[1] + m[5] * v[2]
 	const z = m[6] * v[0] + m[7] * v[1] + m[8] * v[2]
 	return v3Inplace(v, x, y, z)
+}
+
+export const inverseM3 = (m: M3): M3 => {
+	const det =
+		m[0] * (m[4] * m[8] - m[5] * m[7]) -
+		m[1] * (m[3] * m[8] - m[5] * m[6]) +
+		m[2] * (m[3] * m[7] - m[4] * m[6])
+	if (det === 0) {
+		throw new Error('Matrix is not invertible')
+	}
+	const invDet = 1 / det
+	return m3(
+		(m[4] * m[8] - m[5] * m[7]) * invDet,
+		(m[2] * m[7] - m[1] * m[8]) * invDet,
+		(m[1] * m[5] - m[2] * m[4]) * invDet,
+		(m[5] * m[6] - m[3] * m[8]) * invDet,
+		(m[0] * m[8] - m[2] * m[6]) * invDet,
+		(m[2] * m[3] - m[0] * m[5]) * invDet,
+		(m[3] * m[7] - m[4] * m[6]) * invDet,
+		(m[1] * m[6] - m[0] * m[7]) * invDet,
+		(m[0] * m[4] - m[1] * m[3]) * invDet
+	)
+}
+
+export const lerp = (a: Color, b: Color, t: number): Color => {
+	return color(
+		a[0] + (b[0] - a[0]) * t,
+		a[1] + (b[1] - a[1]) * t,
+		a[2] + (b[2] - a[2]) * t,
+		a[3] + (b[3] - a[3]) * t
+	)
+}
+
+export const lerpInplace = (a: Color, b: Color, t: number): Color => {
+	return colorInplace(
+		a,
+		a[0] + (b[0] - a[0]) * t,
+		a[1] + (b[1] - a[1]) * t,
+		a[2] + (b[2] - a[2]) * t,
+		a[3] + (b[3] - a[3]) * t
+	)
+}
+
+export const lerpNumber = (a: number, b: number, t: number): number => {
+	return a + (b - a) * t
 }
